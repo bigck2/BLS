97cd6e03754fe6b1d8c2acb6e1b6795daad9d618
Chris Kiniry <chriskiniry@gmail.com>
2017-11-12 15:36:32 -0600

    Able to pull BLS data from API

---
+++ BLS API CK V1.0.R
@@ -0,0 +1,108 @@
+
+
+# Load Packages -----------------------------------------------------------
+library(jsonlite)
+library(httr)
+
+
+
+# Base Parameters ---------------------------------------------------------
+
+# The BLS URL
+url <- "https://api.bls.gov/publicAPI/v2/timeseries/data/"
+
+my_start_year <- as.character("2017") # Just specifying to make these characters
+my_end_year <- as.character("2017") # Just specifying to make these characters
+my_registration_key <- "416fd9b4813e4a50a808441c4a16d5c9"
+
+# Single Series -----------------------------------------------------------
+
+my_series_id <- "SMS02112600000000001"
+
+# Need to use this one for a signle series
+payload <- paste0("{\"seriesid\":[\"", my_series_id, "\"],
+            \"startyear\":\"", my_start_year, "\",
+            \"endyear\":\"", my_end_year, "\",
+            \"registrationkey\":\"", my_registration_key, "\"}" )
+
+# Send the request (POST)
+r <- POST(url,
+          body = payload,
+          add_headers("content-type" = "application/json"))
+
+
+
+# Multiple Series --------------------------------------------------------------------
+
+# A list of some BLS Series
+some_series <- c("SMS02112600000000001", "SMS02218200000000001", "SMS04223800000000001", 
+                  "SMS04294200000000001", "SMS04380600000000001", "SMS04391400000000001", 
+                  "SMS04460600000000001", "SMS04497400000000001", "SMU02112600000000001", 
+                  "SMU02218200000000001", "SMU04223800000000001", "SMU04294200000000001", 
+                  "SMU04380600000000001", "SMU04391400000000001", "SMU04434200000000001", 
+                  "SMU04460600000000001", "SMU04497400000000001")
+
+# Create list object to hold series of interest and parameters (years, registrationkey)
+my_payload <- list(seriesid = some_series,
+                   startyear = my_start_year,
+                   endyear = my_end_year, 
+                   registrationkey = my_registration_key)
+
+# Convert list into JSON jsonlite::toJSON()
+# We need to specify auto_unbox to TRUE
+my_payload <- toJSON(my_payload, auto_unbox = TRUE, pretty = TRUE)
+
+# Now we can access the BLS API
+r <- POST(url,
+          body = my_payload,
+          add_headers("content-type" = "application/json"))
+
+rm(url, my_payload, some_series, my_series_id, my_start_year, my_end_year)
+
+
+
+# Processing Request ------------------------------------------------------
+
+# These two lines check to make sure the status is 200 and eveything went OK
+http_status(r)
+
+status_code(r) == 200
+
+# We can examine the headers
+# headers(r)
+
+
+
+# Extracting Data ---------------------------------------------------------
+
+# We will use the content function to extract the actual data from the BLS Api
+# This function call will still return a nested list which requires some manipulation
+my_list <- fromJSON(content(r, as = "text"))
+
+# Create a character vector or series id's
+my_series <- my_list[["Results"]][["series"]][["seriesID"]]
+
+# Determine how many rows are in each data frame
+my_rows <- nrow(my_list[["Results"]][["series"]][["data"]][[1]])
+
+# This bind_rows will combine a list of data frames into one data frame
+the_data <- dplyr::bind_rows(my_list[["Results"]][["series"]][["data"]])
+
+# We need to add labels to the_data which specifies the series
+the_data$series_id <- rep(x = my_series, each = my_rows)
+
+rm(my_list, my_series, my_rows, r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
